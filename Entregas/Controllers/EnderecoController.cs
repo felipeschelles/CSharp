d4a2476cef7bf2183e7cfeaee0d63c9b774cb2ff
Felipe <Felipe@CASE>
2018-03-19 17:10:03 -0300

    Adicionando Classes (Fornecedor, Entregador)

---
+++ Entregas/Controllers/EnderecoController.cs
@@ -0,0 +1,95 @@
+﻿using Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllers
+{
+    class EnderecoController
+    {
+        static List<Endereco> MeusEndereco = new List<Endereco>();
+        static int ultimoID = 0;
+        public void SalvarEndereco(Endereco endereco)
+        {
+
+            int id = ultimoID = +1;
+            Endereco.PessoaID = id;
+            MeusEndereco.Add(Endereco);
+
+        }
+
+        public Endereco BuscarClientePorNome(string nome)
+        {
+
+            var c = from x in MeusClientes
+                    where x.Nome.ToLower().Contains(nome.Trim().ToLower())
+                    select x;
+
+            if (c != null)
+            {
+                return c.FirstOrDefault();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Cliente BuscarClientePorID(int ID)
+        {
+
+            var c = from x in MeusClientes
+                    where x.PessoaID.Equals(ID)
+                    select x;
+
+            if (c != null)
+            {
+                return c.FirstOrDefault();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Boolean ExcluirEndereco(int ID)
+        {
+
+            Endereco endereco = BuscarEnderecoPorID(ID);
+
+            if (endereco != null)
+            {
+                MeusEndereco.Remove(endereco);
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+
+        public List<Endereco> ListarEnderecos()
+        {
+            return MeusEndereco;
+        }
+
+
+        public Cliente EditarEndereco(Endereco enderecoEditado)
+        {
+            foreach (Endereco x in MeusEndereco)
+            {
+                if (enderecoEditado.PessoaID == x.PessoaID)
+                {
+
+                }
+            }
+        }
+
+    }
+}
+}
---
+++ Entregas/Controllers/EntregadorController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controllers
+{
+    class EntregadorController
+    {
+        static List<Cliente> MeusClientes = new List<Cliente>();
+        static int ultimoID = 0;
+        public void SalvarCliente(Cliente cliente)
+        {
+
+            int id = ultimoID = +1;
+            cliente.PessoaID = id;
+            MeusClientes.Add(cliente);
+
+        }
+
+        public Cliente BuscarClientePorNome(string nome)
+        {
+
+            var c = from x in MeusClientes
+                    where x.Nome.ToLower().Contains(nome.Trim().ToLower())
+                    select x;
+
+            if (c != null)
+            {
+                return c.FirstOrDefault();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Cliente BuscarClientePorID(int ID)
+        {
+
+            var c = from x in MeusClientes
+                    where x.PessoaID.Equals(ID)
+                    select x;
+
+            if (c != null)
+            {
+                return c.FirstOrDefault();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Boolean ExcluirCliente(int ID)
+        {
+
+            Cliente cliente = BuscarClientePorID(ID);
+
+            if (cliente != null)
+            {
+                MeusClientes.Remove(cliente);
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+
+        public List<Cliente> ListarClientes()
+        {
+            return MeusClientes;
+        }
+
+
+        public Cliente EditarContaCliente(Cliente clienteEditado)
+        {
+            foreach (Cliente x in MeusClientes)
+            {
+                if (clienteEditado.PessoaID == x.PessoaID)
+                {
+
+                }
+            }
+        }
+
+    }
+}
+}
